535c07a4a5b2fc5c2aa39efcb27e8f82683026a2
AaronDai113 <aaron11388@outlook.com>
2016-11-27 18:14:31 +0800

    modified templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    me = User.objects.get(username='aaron')
+    posts = Post.objects.filter(author=me)
+    return render(request,'blog/post_list.html',{'posts':posts})
